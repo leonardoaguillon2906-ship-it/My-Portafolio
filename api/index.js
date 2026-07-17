31ffe6bae6e0b4cc737b9357297ae61c1debb856
leonardoaguillon2906-ship-it <leonardoaguillon2906@gmail.com>
2026-07-17 17:09:55 -0500

    Refactor app import to use path.resolve

--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
-// api/index.js
-const app = require('../server');
+const path = require('path');
+const app = require(path.resolve(process.cwd(), 'server'));
 
 module.exports = (req, res) => {
     return app(req, res);
